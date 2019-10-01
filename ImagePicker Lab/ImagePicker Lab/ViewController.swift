d98f39a90a4003d47fee368fd4a3d792071ecd10
Jason Ruan <jasonruan@pursuit.org>
2019-10-01 14:23:36 -0400

    Adds project with objects on main.storyboard and iboutlets in corresponding view controller

---
+++ ImagePicker Lab/ImagePicker Lab/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ImagePicker Lab
+//
+//  Created by Jason Ruan on 10/1/19.
+//  Copyright © 2019 Jason Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: IBOutlets
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
